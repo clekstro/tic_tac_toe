9e3b88e6d3c3a255aa2cb908161c950d35c3f7a4
Curtis Ekstrom <ce@canvus.io>
2013-04-16 03:28:37 -0700

    remove unnecessary test, refactor validation logic

--- lib/game_state_validation.rb
+++ lib/game_state_validation.rb
@@ -12,7 +12,19 @@ class GameStateValidation
   private
 
   def invalid?
-    forged_history? || no_move? || multiple_moves? || out_of_turn?
+    security_risk? || forged_history? || no_move? || multiple_moves? || out_of_turn?
+  end
+
+  def security_risk?
+    invalid_keys? || invalid_values?
+  end
+
+  def invalid_keys?
+    @passed_state.keys != @saved_state.keys
+  end
+
+  def invalid_values?
+    @passed_state.values.any? { |v| !v.match(/[XO]{1}|\A\Z/) }
   end
 
   def forged_history?
@@ -38,9 +50,17 @@ class GameStateValidation
   end
 
   def out_of_turn?
-    x_turns = @saved_state.select { |k,v| v == "X" }.size
-    o_turns = @passed_state.select { |k,v| v == "O" }.size
-    (o_turns > x_turns) || (x_turns > o_turns + 1)
+    x_out_of_turn? || o_out_of_turn?
+  end
+
+  def o_out_of_turn?
+    value_delta(@saved_state, "O", "X") > 0
+  end
+
+  def x_out_of_turn?
+    prev = value_delta(@saved_state, "X", "O")
+    curr = value_delta(@passed_state, "X", "O")
+    prev + curr >= 2
   end
 
   def pairs_with_values(hash)
@@ -49,6 +69,12 @@ class GameStateValidation
 
   def amount_with_values(hash)
     pairs_with_values(hash).size
+  end
+
+  def value_delta(hash, val1, val2)
+    v1_cnt = hash.select { |k,v| v == val1 }.size
+    v2_cnt = hash.select { |k,v| v == val2 }.size
+    v1_cnt - v2_cnt
   end
 
 end
--- spec/lib/game_state_validation_spec.rb
+++ spec/lib/game_state_validation_spec.rb
@@ -1,25 +1,47 @@
 require_relative '../../lib/game_state_validation'
 
 describe GameStateValidation do
-  let(:history) { {"A1"=> "X", "A2"=> "X", "A3"=> "O" } }
+  let(:history) do
+    {
+      "A1"=>"X",
+      "A2"=>"X",
+      "A3"=>"O" 
+    }
+  end
   context "#invalid?" do
     it "identifies historical inconsistencies" do
-      forged_history = {"A1"=> "X", "A2"=> "O", "A3"=> "X" }
+      forged_history = {
+        "A1"=>"X",
+        "A2"=>"O",
+        "A3"=>"X"
+      }
       GameStateValidation.new(history, forged_history).valid?.should be_false
     end
+
     it "detects lack of move" do
-      no_move = {"A1"=> "X", "A2"=> "X", "A3"=> "O" }
-      GameStateValidation.new(history, no_move).valid?.should be_false
+      GameStateValidation.new(history, history).valid?.should be_false
     end
 
     it "detects more than one move at once" do
-      multiple_moves = {"A1"=> "X", "A2"=> "X", "A3"=> "O", "C1" => "X", "C2" => "X" }
+      multiple_moves = {
+        "A1"=>"X",
+        "A2"=>"X",
+        "A3"=>"O",
+        "C1"=>"X",
+        "C2"=>"X"
+      }
       GameStateValidation.new(history, multiple_moves).valid?.should be_false
     end
 
-    it "detects when user attempts to move for opponent" do
-      out_of_turn = {"A1"=> "X", "A2"=> "X", "A3"=> "O", "C1" => "O", "C3" => "0"}
-      GameStateValidation.new(history, out_of_turn).valid?.should be_false
+    it "detects out of turn move" do
+      x_moves_again = {
+        "A1"=>"X",
+        "A2"=>"X",
+        "A3"=>"O",
+        "B1"=>"X"
+      } 
+      GameStateValidation.new(history, x_moves_again).valid?.should be_false
     end
+
   end
 end
