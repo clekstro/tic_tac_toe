304f128a0f0f7f9bd86a5f8a37105deaeac2ddf1
Curtis Ekstrom <ce@canvus.io>
2013-04-16 03:29:44 -0700

    add basic computer game cukes

---
+++ features/play_computer_game.feature
@@ -0,0 +1,15 @@
+Feature: Play Against Computer
+
+  @javascript
+  Scenario: Game Interactivity For Participant
+    Given I am on the start game menu screen
+    And I choose computer as game type
+    And I make my first move
+    Then my move should be visible
+    And the computer should respond
+    And it should be my turn to play
+
+  Scenario: Game Interactivity for Non-Participant
+    Given someone else started a computer game
+    And I attempt to play it
+    Then I should not be able to access the game
---
+++ features/step_definitions/computer_gameplay.rb
@@ -0,0 +1,40 @@
+require 'uri'
+
+Given(/^I have started a computer game$/) do
+  @game = ComputerGame.create
+  driver = Capybara.current_session.driver
+  driver.set_cookie "game_token", @game.token, domain: "127.0.0.1", path: "/games/#{@game.id}"
+end
+
+Given(/^I make my first move$/) do
+  visit "/games/1"
+  find("#A1").click
+end
+
+Then(/^my move should be visible$/) do
+  within("#A1") do
+    page.should have_text("X")
+  end
+end
+
+Then(/^the computer should respond$/) do
+  within("#gameboard") do
+    page.should have_text("O")
+  end
+end
+
+Then(/^it should be my turn to play$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^someone else started a computer game$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I attempt to play it$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not be able to access the game$/) do
+  pending # express the regexp above with the code you wish you had
+end
